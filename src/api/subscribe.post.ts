bc57d68b4909b7a38df9c076cc603d2c00d29ea5
sdkaraarslan <sdkaraarslan@gmail.com>
2023-08-18 18:35:33 +0200

    feat: Implement basic authentication with environment variables, API endpoint, and login route

--- src/api/subscribe.post.ts
+++ src/api/subscribe.post.ts
@@ -1,35 +1,5 @@
 import http from "node:http";
-
-const readBody = <T>(req: http.IncomingMessage): Promise<T> => {
-  return new Promise((resolve, reject) => {
-    const data: Buffer[] = [];
-
-    if (req.method?.toUpperCase() === "GET" || !req.method) {
-      return resolve({} as T);
-    }
-
-    req
-      .on("error", (err) => {
-        reject(err);
-      })
-      .on("data", (chunk) => {
-        data.push(chunk);
-      })
-      .on("end", () => {
-        const body = Buffer.isBuffer(data) ? Buffer.concat(data) : data;
-        const isUrlEncoded =
-          req.headers["content-type"] === "application/x-www-form-urlencoded";
-
-        if (isUrlEncoded) {
-          return resolve(
-            Object.fromEntries(new URLSearchParams(body.toString("utf-8"))) as T
-          );
-        }
-
-        return resolve(JSON.parse(body.toString("utf-8")));
-      });
-  });
-};
+import { readBody } from "~/lib/read-body";
 
 export default async (req: http.IncomingMessage, res: http.ServerResponse) => {
   const body = await readBody(req);
